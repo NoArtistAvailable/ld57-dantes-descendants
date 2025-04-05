94fba1ea4508c75e2826ff8d27da3d65ae104da2
zach <felix.reinemann@gmail.com>
2025-04-05 14:12:26 +0200

    base for cards

---
+++ LD57-Depths/Assets/Modules/Combat/Card.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace LD57
+{
+	[Serializable]
+	public abstract class Card
+	{
+		public abstract string Name { get; }
+		public abstract string Description { get; }
+	}
+	
+	public static class CardManager
+	{
+		private static List<Card> GetAllCardTypes()
+		{
+			List<Card> allCards = new List<Card>();
+            
+			// Get all types that inherit from Card
+			Type[] cardTypes = Assembly.GetAssembly(typeof(Card))
+				.GetTypes()
+				.Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(Card)))
+				.ToArray();
+            
+			// Create an instance of each card type
+			foreach (Type cardType in cardTypes)
+			{
+				try
+				{
+					Card cardInstance = (Card)Activator.CreateInstance(cardType);
+					allCards.Add(cardInstance);
+				}
+				catch (Exception e)
+				{
+					Debug.LogError($"Failed to create instance of {cardType.Name}: {e.Message}");
+				}
+			}
+            
+			return allCards;
+		}
+		public static List<Card> AllCards = GetAllCardTypes();
+	}
+
+	public class Mod_Healthy : Card, IManipulateHealth
+	{
+		public override string Name => "Healthy";
+		public override string Description => "Increases Health";
+		public float healthMult = 1.5f;
+		public float ManipulateHealth(float value)
+		{
+			return value * healthMult;
+		}
+	}
+	public class Mod_Strong : Card, IManipulatePower
+	{
+		public override string Name => "Strong";
+		public override string Description => "Increases Power";
+		public float powerMult = 1.3f;
+		public float ManipulatePower(float value)
+		{
+			return value * powerMult;
+		}
+	}
+	public class Mod_Smart : Card, IManipulateSpeed
+	{
+		public override string Name => "Smart";
+		public override string Description => "Increases Speed";
+		public float speedMult = 1.2f;
+		public float ManipulateSpeed(float value)
+		{
+			return value * speedMult;
+		}
+	}
+}
---
+++ LD57-Depths/Assets/Modules/Combat/Unit.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace LD57
+{
+    [Serializable]
+    public class Unit
+    {
+        public string name;
+        public Texture2D faceTexture;
+
+        public float baseHealth = 10;
+        public float Health => cards.OfType<IManipulateHealth>()
+            .Aggregate(baseHealth, (current, card) => card.ManipulateHealth(current));
+        
+        public float baseSpeed = 1;
+        public float Speed => cards.OfType<IManipulateSpeed>()
+            .Aggregate(baseHealth, (current, card) => card.ManipulateSpeed(current));
+        
+        public float basePower = 1;
+        public float Power => cards.OfType<IManipulatePower>()
+            .Aggregate(baseHealth, (current, card) => card.ManipulatePower(current));
+        
+        public List<Card> cards = new List<Card>();
+    }
+
+    public interface IManipulateHealth
+    {
+        public float ManipulateHealth(float value);
+    }
+    public interface IManipulateSpeed
+    {
+        public float ManipulateSpeed(float value);
+    }
+    public interface IManipulatePower
+    {
+        public float ManipulatePower(float value);
+    }
+}
